b28964d21c62cee2cd47179cb8899353667b7f1d
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-04-01 19:05:07 +0300

    Update db_query_helper.php

--- app/Helpers/db_query_helper.php
+++ app/Helpers/db_query_helper.php
@@ -49,6 +49,54 @@ function select_one_uni($uni)
 
 	return $arr;
 }
+
+function select_olympiads()
+{
+	$db = db_connect();
+	$query = $db->query('SELECT
+							*
+						FROM
+							olympiad');
+
+	if (!$db->error()) 
+	{
+		throw new \CodeIgniter\Database\Exceptions\DatabaseException();
+	}
+
+	$arr = $query->getResultArray();
+	$db->close();
+
+	return $arr;
+}
+
+function select_one_olympiad($olympiad)
+{
+	$db = db_connect();
+
+	$pQuery = $db->prepare(function ($db)
+	{
+		$sql = 'SELECT
+					*
+				FROM
+					olympiad
+				WHERE
+					short_name=?';
+		return (new Query($db))->setQuery($sql);
+	});
+
+	$result = $pQuery->execute($olympiad);
+
+	if ($pQuery->hasError())
+	{
+		throw new \CodeIgniter\Database\Exceptions\DatabaseException();
+	}
+
+	$arr = $result->getResultArray();
+	$db->close();
+
+	return $arr;
+}
+
 function select_olymp_team_participate($olympiad)
 {
 	$db = db_connect();
