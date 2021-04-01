daa9e3b99aeb82583e3ab77db0d11543a325b6e5
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-04-01 19:03:35 +0300

    db_query_helper.php: Added queries for selecting olympiads

--- app/Helpers/db_query_helper.php
+++ app/Helpers/db_query_helper.php
@@ -49,3 +49,67 @@ function select_one_uni($uni)
 
 	return $arr;
 }
+function select_olymp_team_participate($olympiad)
+{
+	$db = db_connect();
+
+	$pQuery = $db->prepare(function ($db)
+	{
+		$sql = 'SELECT DISTINCT
+					participate.team,
+					person.* 
+				FROM
+					participate
+				LEFT JOIN team ON participate.team = team.name
+				INNER JOIN person ON participate.member = person.pid
+				WHERE
+					participate.olympiad = ?';
+
+		return (new Query($db))->setQuery($sql);
+	});
+
+	$result = $pQuery->execute($olympiad);
+
+	if ($pQuery->hasError())
+	{
+		throw new \CodeIgniter\Database\Exceptions\DatabaseException();
+	}
+
+	$arr = $result->getResultArray();
+	$db->close();
+
+	return $arr;
+}
+
+function select_ranking_olymp($olympiad)
+{
+	$db = db_connect();
+
+	$pQuery = $db->prepare(function ($db)
+	{
+		$sql = 'SELECT
+					*
+				FROM
+					`submission`
+				WHERE
+					`submission`.`olympiad` = ?
+				ORDER BY
+					`submission`.`completed_tasks`
+				DESC ,
+					`submission`.`time_taken`
+				DESC';
+		return (new Query($db))->setQuery($sql);
+	});
+
+	$result = $pQuery->execute($olympiad);
+
+	if ($pQuery->hasError())
+	{
+		throw new \CodeIgniter\Database\Exceptions\DatabaseException();
+	}
+
+	$arr = $result->getResultArray();
+	$db->close();
+
+	return $arr;
+}
