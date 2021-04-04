1cf2113dca0fc995c305047215a7fc2c62e09277
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-04-04 20:28:01 +0300

    Home.php: Add controller logic for olympiad page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,7 +5,9 @@ namespace App\Controllers;
 use function App\Helpers\select_olymp_team;
 use function App\Helpers\select_olymp_team_participate;
 use function App\Helpers\select_olympiads;
+use function App\Helpers\select_participating_members;
 use function App\Helpers\select_participating_unis;
+use function App\Helpers\select_team_coaches;
 use function App\Helpers\select_team_participants;
 use function App\Helpers\select_unis;
 
@@ -43,4 +45,37 @@ class Home extends BaseController
         return view("index", $data);
     }
 
+    public function olympiad($id)
+    {
+        helper(['html', 'db_query_helper']);
+        $link = [
+            'href'  => 'public/css/styles.css',
+            'rel'   => 'stylesheet',
+            'type'  => 'text/css',
+        ];
+
+        $tableSettings = [
+            'table_open' => '<table class="table center" >'
+        ];
+
+        $table = new \CodeIgniter\View\Table();
+        $table->setTemplate($tableSettings);
+        $table->setHeading("Класирания", "№ на отбор", "Участници", "Ръководител");
+
+        $data = [
+            'link' => $link,
+            'table' => $table,
+        ];
+
+        $teams = select_olymp_team($id);
+
+        foreach ($teams as $row) {
+            $participants = ol(select_participating_members($id, $row['name']));
+            $coaches = ol(select_team_coaches($id, $row['name']));
+
+            $table->addRow($row['university'], $row['name'], $participants, $coaches);
+        }
+
+        return view("olympiad", $data);
+    }
 }
