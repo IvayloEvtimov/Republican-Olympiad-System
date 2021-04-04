b6c4786996d64fff88539e1c26c5e43de8c5f563
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-04-04 20:30:00 +0300

    Recreated html files as backend compatible

---
+++ app/Views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Nbu Project</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<?= link_tag($link); ?>
+
+<body>
+    <div class="container">
+        <?= $table->generate(); ?>
+    </div>
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ app/Views/olympiad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Nbu Project</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<?= link_tag($link); ?>
+
+<body>
+    <h2 class="center">Отбори</h2>
+    <div class="container">
+        <?= $table->generate() ?>
+    </div>
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
