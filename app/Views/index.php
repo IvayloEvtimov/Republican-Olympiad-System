25671f9d6b3f5fdf7b5436b075455068e3e24fad
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-04-15 23:25:27 +0300

    index.php: Added links

--- app/Views/index.php
+++ app/Views/index.php
@@ -5,11 +5,22 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <?= link_tag($link); ?>
 
+<header>
+    <nav class="text-center">
+        <ul>
+            <li><?= $olympiad ?></li>
+        </ul>
+    </nav>
+</header>
+
 <body>
     <div class="container">
         <?= $table->generate(); ?>
     </div>
 
+    <footer>
+        <p>Made by F86778 & F98223</p>
+    </footer>
 
     <script src="script.js"></script>
 </body>
