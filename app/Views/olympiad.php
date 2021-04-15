eef2fb63fc31c9e825c50fc7edb9971c5645026a
Ivo Evtimov <ivoevtimov97@abv.bg>
2021-04-15 23:22:56 +0300

    olympiad.php: Added links

--- app/Views/olympiad.php
+++ app/Views/olympiad.php
@@ -4,6 +4,9 @@
 <title>Nbu Project</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <?= link_tag($link); ?>
+<?= link_tag($gstatic_link); ?>
+<?= link_tag($font_link); ?>
+
 
 <body>
     <h2 class="center">Отбори</h2>
